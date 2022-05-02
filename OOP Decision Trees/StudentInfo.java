d72ecf83bb76ef3a6cfd69d9c261c8919cb67e2d
KianZahrai <68393093+Kianzahrai@users.noreply.github.com>
2022-05-02 00:50:01 -0400

    Update StudentInfo.java

--- OOP Decision Trees/StudentInfo.java
+++ OOP Decision Trees/StudentInfo.java
@@ -16,9 +16,9 @@ public class StudentInfo {
 	public static void display() {
 
 		System.out.println("************************************************************");
-		System.out.println("*         											       *");
+		System.out.println("*        Kian Zahrai | Winter 2021			       *");
 		System.out.println("*                                                          *");
-		System.out.println("*     												       *");
+		System.out.println("*     						       *");
 		System.out.println("*                                                          *");
 		System.out.println("************************************************************");
 		System.out.println();
